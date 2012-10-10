1dfd03de61e6cc31147207126861cc14bdea4e3d
gauberR <gauber_r@epita.fr>
2012-10-10 23:18:13 +0200

    Added all files

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using Mogre;
+using Mogre.TutorialFramework;
+using System;
+
+
+
+namespace Game
+{
+    class Main : BaseApplication
+    {
+        public static void Main()
+        {
+            new Main().Go();
+        }
+
+        protected override void CreateScene()
+        {
+            mSceneMgr.AmbientLight = new ColourValue(1, 1, 1);
+            
+            
+        }
+    }
+}
---
+++ Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Mogre;
+
+using Game.Terrain;
+
+namespace Game.PlayerSystem
+{
+    public abstract class Player
+    {
+        protected World mWorldRef;
+        protected Vector3 mCoord;
+        protected SceneManager mSceneMgr;
+        
+        protected string mPLayerName {get; private set;} 
+
+        public Player(ref World worldRef, ref Vector3 coord, ref SceneManager sceneMgr){
+            this.mWorldRef = worldRef;
+            this.mCoord = coord;
+            this.mSceneMgr = sceneMgr;
+        }
+
+        public void addPlayer(string name) { }
+    }
+}
