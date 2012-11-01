724dfcaf5b5115fcb08ece9a23565e51333bd154
GauberR <gauber_r@epita.fr>
2012-11-01 14:54:50 +0100

    Added  manual cube (with subentity)

--- Main.cs
+++ Main.cs
@@ -6,18 +6,80 @@ using System;
 
 namespace Game
 {
-    class Main : BaseApplication
+    class App : BaseApplication
     {
         public static void Main()
         {
-            new Main().Go();
+            new App().Go();
         }
 
         protected override void CreateScene()
         {
-            mSceneMgr.AmbientLight = new ColourValue(1, 1, 1);
-            
-            
+            this.generateBlock();
+            Entity ent = mSceneMgr.CreateEntity("Head", "blockMesh");
+            ent.GetSubEntity(0).SetMaterialName("Cube");
+            ent.GetSubEntity(1).SetMaterialName("Cube2");
+
+            SceneNode node = mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode");
+            node.AttachObject(ent);
+        }
+
+        private void generateBlock()
+        {
+            ManualObject block = new ManualObject("front");
+
+            block.Begin("", RenderOperation.OperationTypes.OT_TRIANGLE_LIST);
+
+            block.Position(new Vector3(0, 0, 0)); block.TextureCoord(1, 1);
+            block.Position(new Vector3(0, 100, 0)); block.TextureCoord(1, 0);
+            block.Position(new Vector3(100, 100, 0)); block.TextureCoord(0, 0);
+            block.Position(new Vector3(100, 0, 0)); block.TextureCoord(0, 1);
+
+            block.Quad(3, 2, 1, 0);
+            block.End();
+
+            block.Begin("", RenderOperation.OperationTypes.OT_TRIANGLE_LIST);
+
+            block.Position(new Vector3(0, 0, -100)); block.TextureCoord(1, 1);
+            block.Position(new Vector3(0, 100, -100)); block.TextureCoord(1, 0);
+            block.Position(new Vector3(100, 100, -100)); block.TextureCoord(0, 0);
+            block.Position(new Vector3(100, 0, -100)); block.TextureCoord(0, 1);
+
+            block.Quad(0, 1, 2, 3);
+            block.End();
+
+            block.Begin("Cube", RenderOperation.OperationTypes.OT_TRIANGLE_LIST);
+
+            block.Position(new Vector3(0, 0, 0)); block.TextureCoord(1, 1);
+            block.Position(new Vector3(0, 100, 0)); block.TextureCoord(1, 0);
+            block.Position(new Vector3(0, 100, -100)); block.TextureCoord(0, 0);
+            block.Position(new Vector3(0, 0, -100)); block.TextureCoord(0, 1);
+
+            block.Quad(0, 1, 2, 3);
+            block.End();
+
+            block.Begin("Cube", RenderOperation.OperationTypes.OT_TRIANGLE_LIST);
+
+            block.Position(new Vector3(100, 0, 0)); block.TextureCoord(1, 1);
+            block.Position(new Vector3(100, 100, 0)); block.TextureCoord(1, 0);
+            block.Position(new Vector3(100, 100, -100)); block.TextureCoord(0, 0);
+            block.Position(new Vector3(100, 0, -100)); block.TextureCoord(0, 1);
+
+            block.Quad(3, 2, 1, 0);
+            block.End();
+
+            block.Begin("Cube", RenderOperation.OperationTypes.OT_TRIANGLE_LIST);
+
+            block.Position(new Vector3(100, 100, 0)); block.TextureCoord(1, 1);
+            block.Position(new Vector3(100, 100, -100)); block.TextureCoord(1, 0);
+            block.Position(new Vector3(0, 100, -100)); block.TextureCoord(0, 0);
+            block.Position(new Vector3(0, 100, 0)); block.TextureCoord(0, 1);
+
+            block.Quad(0, 1, 2, 3);
+            block.End();
+
+
+            block.ConvertToMesh("blockMesh");
         }
     }
 }
