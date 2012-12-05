7f0d9c595ef1e2c264e5b912a2e37694bb1a714e
GauberR <gauber_r@epita.fr>
2012-12-05 23:05:40 +0100

    added Libnoise which is a Perlin3d noise generator

---
+++ Source/World/LibNoise/NoiseQuality.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.LibNoise
+{
+    public enum NoiseQuality {
+	    /**
+	     *  Generates coherent noise quickly.  When a coherent-noise function with
+	     *	this quality setting is used to generate a bump-map image, there are
+	     *  noticeable "creasing" artifacts in the resulting image.  This is
+	     *  because the derivative of that function is discontinuous at integer 
+	     *  boundaries.
+	     */   
+	    FAST,
+	    /**
+	     * Generates standard-quality coherent noise. When a coherent-noise function
+	     * with this quality setting is used to generate a bump-map image, there are
+	     * some minor "creasing" artifacts in the resulting image. This is because
+	     * the second derivative of that function is discontinuous at integer
+	     * boundaries.
+	     */
+	    STANDARD,
+	    /**
+	     * Generates the best-quality coherent noise. When a coherent-noise function
+	     * with this quality setting is used to generate a bump-map image, there are
+	     * no "creasing" artifacts in the resulting image. This is because the first
+	     * and second derivatives of that function are continuous at integer
+	     * boundaries.
+	     */
+	    BEST
+    }
+}
