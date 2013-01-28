b86b6e36b3d491dbca3e5a32b10fb3d5c196c518
Raaeno <aenora.tye@gmail.com>
2013-01-28 19:07:53 +0100

    created option state

--- src/Main/States/Menu/MenuState.cs
+++ src/Main/States/Menu/MenuState.cs
@@ -16,12 +16,13 @@ namespace Game.States
             this.mMenuGUI = new MenuGUI(this.mStateMgr.MiyagiManager, "Menu GUI");
             this.mMenuGUI.SetListener(MenuGUI.Buttons.Play, this.ClickPlayButton);
             this.mMenuGUI.SetListener(MenuGUI.Buttons.Exit, this.ClickExitButton);
+            this.mMenuGUI.SetListener(MenuGUI.Buttons.Options, this.ClickOptionButton);
             Mogre.LogManager.Singleton.DefaultLog.LogMessage("Menu Created");
         }
 
         private void ClickPlayButton(object obj, MouseButtonEventArgs arg) { this.mStateMgr.RequestStatePush(typeof(SecondMenuState)); }
         private void ClickExitButton(object obj, MouseButtonEventArgs arg) { this.mStateMgr.RequestStatePop(); }
-
+        private void ClickOptionButton(object obj, MouseButtonEventArgs arg) { this.mStateMgr.RequestStatePush(typeof(OptionsState)); }
         public override void Update(float frameTime)
         {
             if (this.mStateMgr.Input.IsKeyDown(MOIS.KeyCode.KC_ESCAPE)) { this.mStateMgr.RequestStatePop(); }
---
+++ src/Main/States/Menu/OptionsState.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Miyagi.Common.Events;
+using Miyagi.UI.Controls;
+
+using Game.GUICreator;
+
+namespace Game.States
+{
+    public class OptionsState : State
+    {
+        private OptionsGUI mOptionsGUI;
+        private bool mIsFullScreen;
+        public OptionsState(StateManager stateMgr) : base(stateMgr) { }
+
+        protected override void Startup()
+        {
+            this.mOptionsGUI = new OptionsGUI(this.mStateMgr.MiyagiManager, "Options GUI");
+            this.mOptionsGUI.SetListener(OptionsGUI.ButtonName.FullScreen, this.ClickFullScreenButton);
+            this.mOptionsGUI.SetListener(OptionsGUI.ButtonName.HighQuality, this.ClickQualityButton);
+            this.mOptionsGUI.SetListener(OptionsGUI.ButtonName.Music, this.ClickMusicButton);
+            this.mOptionsGUI.SetListener(OptionsGUI.ButtonName.VSync, this.ClickVSyncButton);
+            this.mOptionsGUI.SetListenerBack(this.ClickBackButton);
+            this.mIsFullScreen = this.mStateMgr.Window.IsFullScreen;
+        }
+
+        public override void Hide()
+        {
+            this.mStateMgr.MiyagiManager.CursorVisibility = false;
+        }
+
+        public override void Show()
+        {
+            this.mOptionsGUI.Show();
+            this.mStateMgr.MiyagiManager.CursorVisibility = true;
+        }
+
+        public override void Update(float frameTime)
+        {
+            if (this.mStateMgr.Input.IsKeyDown(MOIS.KeyCode.KC_ESCAPE)) { this.mStateMgr.RequestStatePop(); }
+        }
+
+        protected override void Shutdown() { this.mOptionsGUI.Dispose(); }
+
+        private void SwitchText(Button b) 
+        {
+            if (b.Text == "ON") {b.Text = "OFF";}
+            else{b.Text = "ON";}
+        }
+
+        private void ClickBackButton(object obj, MouseButtonEventArgs arg) { this.mStateMgr.RequestStatePop();}
+        private void ClickMusicButton(object obj, MouseButtonEventArgs arg) 
+        {
+            this.SwitchText(this.mOptionsGUI.Buttons[OptionsGUI.ButtonName.Music]);
+        }
+        private void ClickQualityButton(object obj, MouseButtonEventArgs arg) 
+        {
+            this.SwitchText(this.mOptionsGUI.Buttons[OptionsGUI.ButtonName.HighQuality]);
+        }
+        private void ClickVSyncButton(object obj, MouseButtonEventArgs arg) 
+        {
+            this.SwitchText(this.mOptionsGUI.Buttons[OptionsGUI.ButtonName.VSync]);
+        }
+        private void ClickFullScreenButton(object obj, MouseButtonEventArgs arg) 
+        {
+            this.mIsFullScreen = !this.mIsFullScreen;
+            this.mStateMgr.Window.SetFullscreen(this.mIsFullScreen, this.mStateMgr.Window.Width, this.mStateMgr.Window.Height);
+            this.SwitchText(this.mOptionsGUI.Buttons[OptionsGUI.ButtonName.FullScreen]);
+        }
+        //private void ClickScreenResolutionButton(object obj, MouseButtonEventArgs arg) { this.mStateMgr.Window.Resize(,); }
+        }
+}
