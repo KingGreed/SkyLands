0b9515c68fff6a7b1cdcbd42963c6e5aefcf723e
Romain Biessy <r.biessy78@gmail.com>
2013-02-25 15:52:16 +0100

    Minor changes

--- src/Main/Characters/CharacMgr.cs
+++ src/Main/Characters/CharacMgr.cs
@@ -29,6 +29,8 @@ namespace Game.CharacSystem
             this.mInput = stateMgr.Input;
             this.mWorld = world;
             this.mCharacList = new List<VanillaCharacter>();
+
+            this.mStateMgr.MyConsole.OnCommandEntered += new MyConsole.ConsoleEvent(this.OnCommandEntered);
         }
 
         public void AddCharacter(CharacterInfo info)
@@ -55,6 +57,63 @@ namespace Game.CharacSystem
         }
 
         public VanillaCharacter GetCharacter(int index = 0) { return this.mCharacList[index]; }    // By default return the main player
+
+        private void OnCommandEntered(string command)
+        {
+            string[] args = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (args.Length >= 2)
+            {
+
+                if (args[0] == "/get_charac_pos")
+                {
+                    int index;
+                    if (int.TryParse(args[1], out index))
+                    {
+                        Vector3 tmp = this.GetCharacter(index).FeetPosition / MainWorld.CUBE_SIDE;
+                        tmp.x = Mogre.Math.IFloor(tmp.x);
+                        tmp.y = Mogre.Math.IFloor(tmp.y);
+                        tmp.z = Mogre.Math.IFloor(tmp.z);
+
+                        this.mStateMgr.WriteOnConsole("FeetPosition : " + '(' + tmp.x + ',' + tmp.y + ',' + tmp.z + ')');
+                    }
+                }
+                else if (args[0] == "/get_charac_yaw")
+                {
+                    int index;
+                    if (int.TryParse(args[1], out index))
+                    {
+                        this.mStateMgr.WriteOnConsole("Yaw : " + this.GetCharacter(index).Node.Orientation.Yaw.ValueAngleUnits);
+                        this.mStateMgr.WriteOnConsole("W : " + this.GetCharacter(index).Node.Orientation.w);
+                        this.mStateMgr.WriteOnConsole("X : " + this.GetCharacter(index).Node.Orientation.x);
+                        this.mStateMgr.WriteOnConsole("Y : " + this.GetCharacter(index).Node.Orientation.y);
+                        this.mStateMgr.WriteOnConsole("Z : " + this.GetCharacter(index).Node.Orientation.z);
+                    }
+                }
+                else if (args[0] == "/flip")
+                {
+                    int index;
+                    if (int.TryParse(args[1], out index) && index < 8)
+                    {
+                        SceneNode node = this.GetCharacter().Points[index];
+                        node.FlipVisibility();
+                        this.mStateMgr.WriteOnConsole("Flipped : " + MyConsole.GetString(node.Position));
+                    }
+                }
+                else if (args[0] == "/goTo")
+                {
+                    LogManager.Singleton.DefaultLog.LogMessage("test");
+                    int index;
+                    if (int.TryParse(args[1], out index))
+                    {
+                        LogManager.Singleton.DefaultLog.LogMessage("test");
+                        this.GetCharacter().moveForward(index);
+                    }
+                }
+            }
+        }
+
+
 
         public void Update(float frameTime)
         {
