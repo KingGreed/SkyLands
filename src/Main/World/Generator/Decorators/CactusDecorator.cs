1da55e66cffa8f347fb63055b7241c629390cae0
gauberR <gauber_r@epita.fr>
2013-02-25 19:26:53 +0100

    added decorators

---
+++ src/Main/World/Generator/Decorators/CactusDecorator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using API.Generator;
+using API.Geo.Cuboid;
+
+using Game.World.Blocks;
+
+namespace Game.World.Generator.Decorators
+{
+    class CactusDecorator : Decorator {
+        public CactusDecorator() {}
+
+
+        public override void populate(Island curr, Random random)
+        {
+            for (int i = 0; i < (curr.getSize().x + curr.getSize().z) * 4 ; i++)
+            {
+                int x = random.Next() % (int)(curr.getSize().x * MainWorld.CHUNK_SIDE);
+                int z = random.Next() % (int)(curr.getSize().z * MainWorld.CHUNK_SIDE);
+                int y = curr.getSurfaceHeight(x, z);
+
+                while(y == -1 && curr.getBlock(x, y, z, true) is GrassBlock) {
+                    x = random.Next() % (int)(curr.getSize().x * MainWorld.CHUNK_SIDE);
+                    z = random.Next() % (int)(curr.getSize().z * MainWorld.CHUNK_SIDE);
+                    y = curr.getSurfaceHeight(x, z);
+                }
+
+                curr.setBlockAt(x, y, z, 8, true);
+                if (random.Next(3)  > 1) { curr.setBlockAt(x, y + 1, z, 8, true); }
+                if (random.Next(9)  > 1) { curr.setBlockAt(x, y + 2, z, 8, true); }
+                if (random.Next(17) > 1) { curr.setBlockAt(x, y + 3, z, 8, true); }
+
+            }
+        }
+    }
+}
