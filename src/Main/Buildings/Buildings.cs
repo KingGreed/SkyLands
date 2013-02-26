1cebc2b69f79709622ba6bf073b92e84261ec3d7
Romain Biessy <r.biessy78@gmail.com>
2013-02-26 13:35:48 +0100

    Minor changes

---
+++ src/Main/Buildings/Buildings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Mogre;
+
+using API.Geo.Cuboid;
+
+namespace Game.Buildings
+{
+    public class Buildings
+    {
+        private Vector3 mPos;
+        private Island mIsland;
+        
+        public Buildings(Island island, Vector3 pos)
+        {
+            this.mPos = pos;
+            this.mIsland = island;
+            this.Build();
+        }
+
+        private void Build()
+        {
+            /*for (int y = 5; y > 0; y--)
+            {
+                for(int x = 0; x <
+            }
+
+            for (int y = 1; y <= 5; y++)
+            {
+                int side = 2 * y + 1;
+            }*/
+        }
+    }
+}
