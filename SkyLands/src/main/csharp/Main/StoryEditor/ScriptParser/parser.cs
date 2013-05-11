09ba2d177ffe6049b748f3638440ee0839980526
gauberR <gauber_r@epita.fr>
2013-05-11 18:41:23 +0200

    Added begining of the story editor

---
+++ SkyLands/src/main/csharp/Main/StoryEditor/ScriptParser/parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mogre;
+
+namespace Game.StoryEditor.ScriptParser {
+    class parser {
+        public List<Vector3> v;
+        private ItemMgr i;
+
+        public parser() { this.v = new List<Vector3>(); }
+        public parser(string Scenario) {
+            
+        }
+
+
+
+    }
+}
