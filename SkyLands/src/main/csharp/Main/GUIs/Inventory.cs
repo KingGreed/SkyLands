74348c12039698b1fe3f9a6b4ea0ccba5ceefeba
gauberR <gauber_r@epita.fr>
2013-06-12 17:32:42 +0200

    Added Inventory

---
+++ SkyLands/src/main/csharp/Main/GUIs/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using Awesomium.Windows.Forms;
+
+using Game.BaseApp;
+
+using Mogre;
+
+namespace Game.GUIs
+{
+    public class Inventory : GUI {
+
+        public Inventory()
+            : base(new Vector2(0, 0), new Vector2(350, 330), "file://" + Directory.GetCurrentDirectory() + "/media/web/inventory.html") {
+        }
+
+        public override void onDocumentReady(object sender, Awesomium.Core.UrlEventArgs e) {
+            OgreForm.webView.ExecuteJavascript("resize(2, 2)");
+        }
+    }
+}
