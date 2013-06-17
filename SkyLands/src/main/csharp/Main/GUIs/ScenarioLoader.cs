b83e193136c6197d8ee760d31bf2f1a7fc082dcf
gauberR <gauber_r@epita.fr>
2013-06-17 17:56:23 +0200

    Update of GUI

--- SkyLands/src/main/csharp/Main/GUIs/ScenarioLoader.cs
+++ SkyLands/src/main/csharp/Main/GUIs/ScenarioLoader.cs
@@ -8,17 +8,17 @@ using System.Windows.Forms;
 using Game.BaseApp;
 
 namespace Game.GUIs {
-    public class PlayMenu : GUI {
+    public class ScenarioLoader : GUI {
         private readonly StateManager mStateMgr;
 
-        public PlayMenu(StateManager stateMgr)
+        public ScenarioLoader(StateManager stateMgr)
             : base(new Vector2(0, 0), new Vector2(404, 404), "play.html", DockStyle.Fill) {
             this.mStateMgr = stateMgr;
         }
 
         public override void onDocumentReady(object sender, UrlEventArgs e) {
             base.onDocumentReady(sender, e);
-            JSObject j = OgreForm.webView.CreateGlobalJavascriptObject("PlayMenuJSObject");
+            JSObject j = OgreForm.webView.CreateGlobalJavascriptObject("ScenarioLoaderObject");
             j.Bind("back", false, HideAndPlay);
             j.Bind("play", false, (s, args) => this.mStateMgr.RequestStatePush(typeof(GameState)));
             j.Bind("load", false, Load);
