dacf4540980262fa2e13652b328e71a9762bfbeb
gauberR <gauber_r@epita.fr>
2013-06-17 15:53:30 +0200

    Minor changes

--- SkyLands/src/main/csharp/Main/World/Blocks/CrystalGateH.cs
+++ SkyLands/src/main/csharp/Main/World/Blocks/CrystalGateH.cs
@@ -2,15 +2,13 @@
 
 namespace Game.World.Blocks {
     class CrystalGateH : CrystalGate, Air, TransparentBlock {
+        
         public CrystalGateH() {
-
             string[] s = new string[]{ "red", "blue" };
-
             this.mName = "CrystalGate H";
             this.mMaterial = "cube/CrystalGate/" + s[new Random().Next(0, s.Length)];
             this.mId = 15;
             this.mMeshType = 0;
         }
-
     }
 }
