f340b995a52f745d475c6a2b94dbf413497f5158
gauberR <gauber_r@epita.fr>
2013-06-17 15:54:10 +0200

    Minor changes

---
+++ SkyLands/src/main/csharp/Main/World/Generator/Decorators/Cavern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using API.Generator;
+using API.Geo.Cuboid;
+
+using Game.World.Blocks;
+
+using Mogre;
+
+namespace Game.World.Generator.Decorators
+{
+    class Pine : Decorator {
+        public override void populate(Island curr, Random random) {
+
+        }
+    }
+}
